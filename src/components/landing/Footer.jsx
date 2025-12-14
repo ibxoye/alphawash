e5132e2f259e7570f8a06e04b2e211e022b2cd68
ib <oio3735@mavs.uta.edu>
2025-12-14 14:41:52 -0600

    Added GoFundMe link

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -17,6 +17,9 @@ export default function Footer() {
           <a className="text-zinc-500 hover:text-white" href="https://www.tiktok.com/@alpha_wash?_r=1&_t=ZP-925nzrqWHMd">
             TikTok
           </a>
+          <a className="text-zinc-500 hover:text-white" href="https://www.gofundme.com/f/bringing-alpha-wash-to-life-a-bold-mens-laundry-brand?attribution_id=sl:e1a1cb92-2c8f-488e-a898-358a7c8f021d&lang=en_US&ts=1763939188&utm_campaign=fp_sharesheet&utm_content=amp17_tc&utm_medium=customer&utm_source=copy_link">
+            GoFundMe
+          </a>
         </div>
       </div>
     </footer>
